6a4e61846f217c2267aded5664fcf4cbef2eef7f
jacobsparks <38540611+jacobsparks@users.noreply.github.com>
2020-09-04 09:39:45 -0400

    added more command prompt commands

--- arya.py
+++ arya.py
@@ -1,3 +1,5 @@
+#TODO: figure out how to open command line as administrator for "execute a command" task
+#TODO: add email sending
 #TODO: move tasks to seperate file, call functions as needed in main
 #TODO: add calendar-keeping
 #TODO: add shut down computer
@@ -40,28 +42,56 @@ def get_speech(): #gets user input, converts to string, checks to see if input i
 def arya_tasks(voice): #all of arya's functionality
     if 'what is your name' in voice or 'what\'s your name' in voice:
         arya_voice('My name is Arya')
-    if 'Aria what time is it' in voice:
+    elif 'Aria what time is it' in voice:
         time.ctime()
         arya_voice('The current time is ' + str(time.strftime('%I %M%p %Z')))
-    if 'Aria what is the date' in voice:
+    elif 'Aria what is the date' in voice:
         arya_voice('The current date is ' + str(time.strftime('%B %d %Y')))
-    if 'Aria search' in voice:
+    elif 'Aria search' in voice:
         arya_voice('What would you like to search for?')
         search = get_speech()
         webbrowser.get().open('https://google.com/search?q=' + search)
         arya_voice('Sure thing. Here is what I found for ' + search)
-    if 'Aria find location' in voice:
+    elif 'Aria find location' in voice:
         arya_voice('Okay. Please specify a location')
         location = get_speech()
         webbrowser.get().open('https://www.google.com/maps/search/' + location)
         arya_voice('Sure thing. Locating ' + location)
-    if 'Aria run command ipconfig' in voice:
-        arya_voice('Running ipconfig')
-        os.system('start cmd /k ipconfig')
-    if 'Aria text my phone' in voice:
+    elif 'Aria execute a command' in voice:
+        arya_voice('What command would you like to execute?')
+        command = get_speech()
+        if 'ipconfig' in command:
+            os.system('start cmd /k ipconfig')
+        elif 'netstat' in command:
+            os.system('start cmd /k netstat')
+        elif 'traceroute' in command:
+            os.system('start cmd /k tracert')
+        elif 'ping' in command:
+            arya_voice('What destination would you like to ping?')
+            destination = get_speech()
+            print(destination)
+            os.system('start cmd /k ping ' + destination.replace(' ', ''))
+        elif 'driver query' in command:
+            os.system('start cmd /k driverquery')
+        elif 'task list' in command:
+            os.system('start cmd /k tasklist')
+        elif 'taskkill' in command:
+            arya_voice('Okay. Do you want to kill the task based off of the process ID or image name?')
+            dec = get_speech()
+            if dec == 'process ID':
+                arya_voice('Please give me the process ID')
+                pid = get_speech()
+                os.system('start cmd /k taskkill -pid ' + pid)
+            elif dec == 'image name':
+                arya_voice('Please give me the image name')
+                img_name = get_speech()
+                os.system('start cmd /k taskkill -im ' + img_name)
+        elif 'system info' in command:
+            os.system('start cmd /k systeminfo')
+    elif 'Aria text my phone' in voice:
         arya_voice('Will do. What would you like it to say?')
-        account_sid = 'AC2305d05ea331fb846dfb0e56b279eb4a'
-        auth_token = '87818234b0f3483f0be75eb7e86d0fde'
+        account_sid = '*************************'
+        auth_token = '**************************'
         client = Client(account_sid, auth_token)
         message = client.messages.create(
             from_='+13343784883',
@@ -69,7 +99,7 @@ def arya_tasks(voice): #all of arya's functionality
             to='+17066768585'
         )
         arya_voice('Text successfully sent. What would you like me to do now?')
-    if 'Aria let\'s play a number guessing game' in voice:
+    elif 'Aria let\'s play a number guessing game' in voice:
         arya_voice('Okay. Lets do it. You\'re going down! Pick a number between 1 and 20. You get 5 tries.')
         num = random.randint(1, 20)
         tries = 0
@@ -86,12 +116,12 @@ def arya_tasks(voice): #all of arya's functionality
             arya_voice('Congrats. You win. What else can I do for you?')
         else:
             arya_voice('You\'ve tried too many times. You lose! What else can I do for you?')
-    if 'Aria translate a word to Pig Latin' in voice:
+    elif 'Aria translate a word to Pig Latin' in voice:
         arya_voice('Sure. What is the word?')
         word = get_speech()
         new_word = word[1:] + word[0] + 'ay'
         arya_voice(word +' in pig latin is ' + new_word)
-    if 'Aria look up a stock price' in voice:
+    elif 'Aria look up a stock price' in voice:
         stock_scrape()
         while True:
             arya_voice('Do you want to lookup another ticker? Yes or no.')
@@ -101,16 +131,16 @@ def arya_tasks(voice): #all of arya's functionality
             else:
                 arya_voice('Okay. What would you like me to do now?')
                 break
-    if 'Aria enter test result' in voice:
+    elif 'Aria enter test result' in voice:
         quality_db()
         arya_voice('The test result has been sent to the database.')
-    if 'Aria exit' in voice:
+    elif 'Aria exit' in voice:
         arya_voice('Goodbye.')
         exit()
     
 def stock_scrape():
     arya_voice('What ticker do you want me to find?')
-    ticker = get_speech()
+    ticker = get_speech().replace(' ', '')
     url = 'https://finance.yahoo.com/quote/' + ticker + '?p=' + ticker + '&.tsrc=fin-srch'
     req = requests.get(url).text
     soup = BeautifulSoup(req, 'html.parser' )
@@ -130,7 +160,7 @@ def quality_db():
     test = get_speech()
     arya_voice('Did the test pass or fail?')
     result = get_speech()
-    db = mysql.connector.connect(host='localhost', user='root', password='*********', database='testdatabase')
+    db = mysql.connector.connect(host='localhost', user='root', password='***********', database='testdatabase')
     cursor = db.cursor()
     sql = 'INSERT INTO qualitytest (RollNum, StyleNum, ColorNum, TestType, PassFail) VALUES (%s,%s,%s,%s,%s)'
     args = (roll, style, color, test, result)
@@ -142,5 +172,5 @@ def quality_db():
 arya_voice('Hey! What can I do for you?')
 while True: #loops program
     voice = get_speech()
-    # print(voice)
+    print(voice)            #used for debugging
     arya_tasks(voice)
